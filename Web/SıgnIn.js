bcd74654733773421723ab0b6044166ddf1212e9
ademvnc <ademmavanaci@gmail.com>
2024-01-07 13:46:19 +0300

    Web Update

---
+++ Web/SıgnIn.js
@@ -0,0 +1,128 @@
+function githubLogin() {
+    const client_id = '4093b88e1fcd28532c8a'; // GitHub Developer Portal'dan alacağınız client ID
+    const redirect_uri = 'http://127.0.0.1:5500/main.html'; // GitHub Developer Portal'dan ayarladığınız geri dönüş URL'si
+    // GitHub'a yönlendirme için URL oluşturma
+    const githubAuthUrl = `https://github.com/login/oauth/authorize?client_id=${client_id}&redirect_uri=${redirect_uri}&scope=user`;
+
+    // Kullanıcıyı GitHub kimlik doğrulama sayfasına yönlendirme
+    window.location.replace(githubAuthUrl);
+}
+function login() {
+
+    //
+    //kontorllerden sonra
+    var userInfo = {
+        email: email,
+        password: password
+    };
+
+
+
+
+
+
+    // Convert the object to a JSON string and save it to localStorage
+    localStorage.setItem("userInfo", JSON.stringify(userInfo));
+
+
+
+}
+
+document.getElementById("signInForm").addEventListener("submit", login);
+
+// Örnek kullanıcı bilgileri
+
+
+async function login(event) {
+
+    event.preventDefault(); // Formun varsayılan davranışını engelle
+
+    const apiUrl = 'https://nodejs-mysql-api-sand.vercel.app/api/v1/auth/login'; // API'nizin gerçek adresiyle değiştirin
+
+    // Kullanıcı girişi için örnek veri
+    const loginData = {
+        email: document.getElementById("email").value, password: document.getElementById("password").value,
+    };
+    console.log(loginData);
+
+    // Giriş isteğini gönder
+    fetch(apiUrl, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(loginData),
+    })
+        .then(response => response.json())
+        .then(data => {
+            if (data.accessToken) {
+                var currentUser = { userId: data.data.userId, email: loginData.email, password: loginData.password }
+                saveUserLocally(currentUser);
+                console.log('Giriş başarılı!');
+                console.log('Access Token:', data.accessToken);
+                console.log('User ID:', data.data.userId);
+                console.log('Name:', data.data.name);
+                console.log('Email:', data.data.email);
+                window.location.href = "main.html";
+                var redirectUrl = 'main.html';
+                // Yönlendirme işlemi
+                window.location.href = redirectUrl;
+            } else {
+                console.error('Giriş başarısız:', data.error);
+            }
+        })
+        .catch(error => {
+            console.error('İstek hatası:', error);
+        });
+      
+}
+// Kullanıcı girişi yapma fonksiyonu
+
+
+// Kullanıcıyı kaydetme fonksiyonu
+function saveUserLocally(currentUser) {
+    if (currentUser) {
+        // Kullanıcı bilgilerini yerel depolamaya kaydet
+        localStorage.setItem('currentUser', JSON.stringify(currentUser));
+        console.log('User saved locally:', currentUser);
+    } else {
+        console.error('No user logged in.');
+    }
+}
+
+// Kullanıcıyı yerel depodan yükleme fonksiyonu
+function loadUserLocally() {
+    const storedUser = localStorage.getItem('currentUser');
+    if (storedUser) {
+        currentUser = JSON.parse(storedUser);
+        console.log('User loaded from local storage:', currentUser);
+    } else {
+        localStorage.setItem()
+    }
+}
+
+
+function validateForm() {
+    var email = document.getElementById('email').value;
+    var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (!emailRegex.test(email)) {
+        alert("Please enter a valid email address.");
+        return false; // prevent form submission
+    }
+
+    // Continue with form submission if email is valid
+    return true;
+}
+
+
+
+// Örnek kullanıcı girişi
+//loginUser('ozdemiroguzhan55@gmail.com', '123456');
+
+// Kullanıcıyı yerel depolamaya kaydet
+//saveUserLocally();
+
+// Kullanıcıyı yerel depodan yükle
+
+// Çağırma örneği
---
+++ Web/SıgnUp.js
@@ -0,0 +1,138 @@
+
+var user = {
+    email: "",
+    password: "",
+    name: "",
+    surname: "",
+    phone: "",
+    numberOfRooms: 0,
+    active: 0
+};
+
+document.getElementById("signUpForm").addEventListener("submit", kayitOl);
+async function kayitOl(event) {
+    event.preventDefault(); // Formun varsayılan davranışını engelle
+
+    // Formdaki değerleri al
+    user.name = document.getElementById("ad").value;
+    user.surname = document.getElementById("soyAd").value;
+    user.email = document.getElementById("email").value;
+    user.password = document.getElementById("passWord").value;
+    user.phone = document.getElementById("phone").value;
+    var confirmPassWord = document.getElementById("confirmPassWord").value;
+
+    console.log(user);
+    /*var ad = document.getElementById("ad").value;
+    var soyAd = document.getElementById("soyAd").value;
+    var email = document.getElementById("email").value;
+    var phone = document.getElementById("phone").value;
+    var password = document.getElementById("passWord").value;
+    var confirmPassWord = document.getElementById("passwordd").value;
+    var nrooms = 0;
+    var active = 0;
+*/
+    // Burada diğer gerekli kontrolleri ekleyebilirsiniz, örneğin şifre güvenlik kontrolleri***
+    // Şifrelerin eşleşip eşleşmediğini kontrol et
+    if (user.password !== confirmPassWord) {
+        alert("Şifreler uyuşmuyor!");
+        return;
+    }
+    // POST isteği için veri objesini oluştur
+    var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(user.email)) {
+        alert("Please enter a valid email address.");
+        return false; // prevent form submission
+    }
+
+    try {
+        const response = await fetch('https://nodejs-mysql-api-sand.vercel.app/api/v1/auth/register', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(user),
+        });
+
+        const result = await response.json();
+
+        if (response.ok) {
+            console.log('Durum mesajı', result);
+            if (result.message == "Registration successful") {
+                alert("Kayıt başarılı");
+                window.location.href = "signin.html";
+            }
+        } else {
+            console.error('Kullanıcı kaydedilirken bir hata oluştu:', result);
+        }
+    } catch (error) {
+        console.error('Error:', error);
+        document.getElementById('result').innerText = 'An error occurred.';
+    }
+    var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(user.email)) {
+        alert("Please enter a valid email address.");
+        return false; // prevent form submission
+    }
+    return true;
+};
+
+
+
+
+function kayitTamamla() {
+    var name = document.getElementById('ad').value;
+    var surname = document.getElementById('soyAd').value;
+    var email = document.getElementById('email').value;
+    var password = document.getElementById('passWord').value;
+    var nrooms = document.getElementById('odaSayisi').value;
+    // Kullanıcı bilgilerini localStorage'e kaydet
+    localStorage.setItem('ad', name);
+    localStorage.setItem('soyAd', surname);
+    localStorage.setItem('email', email);
+    localStorage.setItem('pasWord', password);
+    localStorage.setItem('odaSayisi', nrooms)
+    // Bir sonraki sayfaya yönlendir
+    window.location.href = 'main.html';
+}
+
+function getOptionValue() {
+    console.log("getOptionValue function is called");
+    var select = document.getElementById("odaSayisi");
+    var selectedValue = select.options[select.selectedIndex].value;
+    console.log(selectedValue)
+    var radioContainer = document.getElementById("radioContainer");
+    radioContainer.innerHTML = ''; // Önceki radio butonlarını temizle
+    for (var i = 1; i <= selectedValue; i++) {
+        // Create a new select element
+        var dropdown = document.createElement("select");
+
+        // Set a unique ID for each dropdown
+        dropdown.id = "dropdown" + i;
+
+        // Add options to the dropdown
+        dropdown.innerHTML = `
+        <option value="apartment${i}">Room ${i}</option>
+        <option value="house${i}">Mutfak ${i}</option>
+        <option value="condo${i}">OturmaOdası ${i}</option>
+        <option value="apartment${i}">Yatak Odası ${i}</option>
+        <option value="house${i}">Çocuk Odası ${i}</option>
+        <option value="condo${i}">Çalışma Odası ${i}</option>
+        `;
+
+        // Append the dropdown to the container
+        document.getElementById("form").appendChild(dropdown);
+    }
+}
+
+function validateForm() {
+    var email = document.getElementById('email').value;
+    var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (!emailRegex.test(email)) {
+        alert("Please enter a valid email address.");
+        return false; // prevent form submission
+    }
+
+    // Continue with form submission if email is valid
+    return true;
+}
